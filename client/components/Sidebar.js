463df65f53ccc61b9d2291fef364c27a6e4ae4cd
devshubham.work4u <devshubham.work4u@gmail.com>
2022-03-10 16:04:55 +0530

    css

---
+++ client/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Sidebar.module.css'
+import { useEffect,useState } from 'react'
+import {useRouter} from 'next/router'
+
+
+const Sidebar = () => {
+  return (
+    <div className={StyleSheet.wrapper}>Sidebar</div>
+  )
+}
+
+export default Sidebar
