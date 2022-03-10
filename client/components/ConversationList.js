ba5557263c95e280c7ad9d982f3ae9b973b049c9
devshubham.work4u <devshubham.work4u@gmail.com>
2022-03-11 00:09:19 +0530

    sidebar-icons-set

---
+++ client/components/ConversationList.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import styles from '../styles/conversationlist.module.css'
+
+const ConversationList = () => {
+  return (
+    <div>ConversationList</div>
+  )
+}
+
+export default ConversationList
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Sidebar from '../components/Sidebar'
 
+
 export default function Home() {
   return (
     <div className={styles.wrapper}>
